654392692939ec7d6e374ba90c44c8db6fe2673e
ylu <ylu@ert.com>
2021-02-23 22:25:03 -0500

    add ai

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import {useState} from 'react'
+import {useState, useEffect } from 'react'
 import Head from 'next/head'
+import alanBtn from '@alan-ai/alan-sdk-web';
 import styles from '../styles/Home.module.css'
 
 interface itemType {
@@ -8,22 +9,25 @@ interface itemType {
   category: string;
 }
 
-const menuItems = [
-  {name: "Angus Burger", price: 8.99, category: 'burger'},
-  {name: "Tuna Steak Burger", price: 15.00, category: 'burger'},
-  {name: "Bacon Burger", price: 11.50, category: 'burger'},
-  {name: "Southwest Chicken Burger", price: 9.99, category: 'burger'},
-  {name: "Mozzarella Burger", price: 12.50, category: 'burger'},
-  {name: "Cesar Salad", price: 6.50, category: 'salad'},
-  {name: "BBQ Chicken Salad", price: 13.99, category: 'salad'},
-  {name: "Garden Salad", price: 9.99, category: 'salad'},
-  {name: "Veggie Lasagna", price: 17.99, category: 'pasta'},
-  {name: "Spaghetti & Meatballs", price: 17.99, category: 'pasta'},
-  {name: "Fettuccine Alfredo", price: 17.99, category: 'pasta'},
-];
+interface MyCommandDataType {
+  command: string;
+  data: itemType[];
+}
 
 export default function Home() {
   const [cart, setCart] = useState<itemType[]>([])
+  const [menuItems, setMenuItems] = useState<itemType[]>([])
+
+  useEffect(() => {
+    alanBtn({
+      key: process.env.ALAN_AI_KEY!,
+      onCommand: (commandData: MyCommandDataType) => {
+        if (commandData.command === 'getMenu') {
+          setMenuItems(commandData.data)
+        }
+      },
+    });
+  }, []);
 
   const addToCart = (menuItem: itemType) => {
     setCart((oldCart) => {
